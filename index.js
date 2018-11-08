d420e51caa5d1f797735daa5b696ca16eaeab525
Phumrapee Limpianchop <git@rayriffy.com>
2018-11-08 11:38:43 +0700

    :tada: Intitial C

---
+++ index.js
@@ -0,0 +1,77 @@
+import bodyparser from 'body-parser'
+import dotenv from 'dotenv'
+import db from 'mysql'
+import express from 'express'
+
+// Configuration
+
+const app = express()
+
+app.use(bodyparser.json())
+
+dotenv.config()
+
+const connection = db.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+// Process
+
+app.post('/api', (req, res) => {
+  const category = req.body.req
+  const data = []
+
+  connection.connect()
+
+  connection.query('SELECT `name_en`, `name_jp`, `artist_en`, `artist_jp`, `image_url`, `version`, `bpm`, `level_easy`, `level_basic`, `level_advanced`, `level_expert`, `level_master`, `level_remaster`, `listen_youtube`, `listen_niconico`, `regionlocked` FROM `' + category + '` WHERE 1 ORDER BY RAND() LIMIT 5', (err, rows, fields) => {
+    if (err) {
+      data.push({
+        "reponse": "error",
+        "remark": err
+      })
+    }
+    else {
+      rows = JSON.parse(JSON.stringify(rows))
+      rows.forEach(row => {
+        console.log(row.name_en)
+        data.push(
+          {
+            "name": {
+              "en": row.name_en,
+              "jp": row.name_jp
+            },
+            "artist": {
+              "en": row.artist_en,
+              "jp": row.artist_jp
+            },
+            "image_url": row.image_url,
+            "version": row.version,
+            "bpm": row.bpm,
+            "level": {
+              "easy": row.level_easy,
+              "basic": row.level_basic,
+              "advanced": row.level_advanced,
+              "expert": row.level_expert,
+              "master": row.level_master,
+              "remaster": row.level_remaster
+            },
+            "listen": {
+              "youtube": row.listen_youtube,
+              "niconico": row.listen_niconico
+            },
+            "regionlocked": row.regionlocked
+          }
+        )
+      })
+    }
+    res.send(data);
+  })
+
+  connection.end()
+})
+
+app.listen(process.env.PORT);
