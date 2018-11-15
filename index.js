1c6aa08075d92fb5af96f602455b51a9de997013
Phumrapee Limpianchop <git@rayriffy.com>
2018-11-15 20:51:49 +0700

    error logging

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ app.post('/api', (req, res) => {
   const category = req.body.req
   let data = []
 
-  connection.connect()
+  connection.connect((err) => {
+    if (err)
+      console.log(err)
+  })
 
   connection.query('SELECT `name_en`, `name_jp`, `artist_en`, `artist_jp`, `image_url`, `version`, `bpm`, `level_easy`, `level_basic`, `level_advanced`, `level_expert`, `level_master`, `level_remaster`, `listen_youtube`, `listen_niconico`, `regionlocked` FROM `' + category + '` WHERE 1 ORDER BY RAND() LIMIT 5', (err, rows, fields) => {
     if (err) {
@@ -68,10 +71,11 @@ app.post('/api', (req, res) => {
         )
       })
     }
+
+    connection.end()
+
     res.send(data);
   })
 })
-
-connection.end()
 
 app.listen(process.env.PORT);
